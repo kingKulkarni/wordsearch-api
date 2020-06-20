ee15a665d66f216ea3a6f26b3d1381b5950bcc9f
Avi <Avi@Avi-PC>
2020-06-21 00:35:50 +0530

    Development Branch Commit

--- wordsearch-api/src/main/java/com/avidroid/wordsearchapi/WordsearchApiApplication.java
+++ wordsearch-api/src/main/java/com/avidroid/wordsearchapi/WordsearchApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// Author : Avinash Kulkarni
+// Author : Avinash Kulkarni.
 
 @SpringBootApplication
 public class WordsearchApiApplication {
